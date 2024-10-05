092cd6c92568bac00710181aa3b6a2f1798f9af9
nmogoko <+>
2024-10-05 15:33:04 +0100

    created a simple flask application

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,12 @@
 #  This is the entry point of our application
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
