ca8ce52ed8a43febff17365f34856d52017351c0
Remmy4873 <Remmykiplangat4873@gmail.com>
2024-10-17 21:51:18 +0300

    worked on autherntication

--- backend/src/config.py
+++ backend/src/config.py
@@ -2,4 +2,12 @@
 class Config:
     SQLALCHEMY_DATABASE_URI = "postgresql://postgres_user:yUbydg8bYK5GujDNfRy5fyRjhuvVNJus@dpg-cs0kerggph6c73aa9nig-a.oregon-postgres.render.com:5432/group_four"
     SQLALCHEMY_TRACK_MODIFICATION = False
-    JWT_SECRET_KEY = 'gsjdhjhsjjldksduehehks'
+    JWT_SECRET_KEY = '3259a843-5011-4b6c-8880-54b029aaa069'
+    JWT_BLACKLIST_ENABLED = True
+    JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USERNAME = 'remmykiplangat4873@gmail.com'
+    MAIL_PASSWORD = 'shcheihckaenzzkw'
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
